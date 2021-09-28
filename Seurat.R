c6402fe74e551a1e674a47537b199126010756f6
ntukirafang <61813818+ntukirafang@users.noreply.github.com>
2021-09-28 09:59:35 +0800

    Update Seurat.R

--- Seurat.R
+++ Seurat.R
@@ -1,13 +1,3 @@
-knitr::opts_chunk$set(echo = TRUE)
-#Install cellranger to caculate dataset matrix
-#Install GRch38 database
-##shell
-#wget -O cellranger-3.1.0.tar.gz "https://cf.10xgenomics.com/releases/cell-exp/cellranger-3.1.0.tar.gz?Expires=1568198894&Policy=eyJTdGF0ZW1lbnQiOlt7IlJlc291cmNlIjoiaHR0cDovL2NmLjEweGdlbm9taWNzLmNvbS9yZWxlYXNlcy9jZWxsLWV4cC9jZWxscmFuZ2VyLTMuMS4wLnRhci5neiIsIkNvbmRpdGlvbiI6eyJEYXRlTGVzc1RoYW4iOnsiQVdTOkVwb2NoVGltZSI6MTU2ODE5ODg5NH19fV19&Signature=MAsku-zG5j7hV9svst0pxw9WTJk3ilq3DmLb1obNHmjsOcIL8CF2T0UzYpiGQ2lTQraiPNQ2nzhLgH1liGP~7JjoZ~HousFx1w7POggIWYkhDu7ktlGQCVvjxBqZvfoWpgVvi1U4lpy7Ud2c0Hq1r3HphUH-ZiHqiGSKfTarUZEQOLLx~5CXz~moIM~TwAF0KDLwbAPkceylZUOTcdzBAH~IsD-LBXK-7WbBA5ZOFOXhAtrLWQnzCRU0HjnkXRUNtKGAJO7ApXkg8PcNQaEWBGiJ6hrnCsbPdjaVFhOcRd-6mtV-zg2WTt1kVcd8UjA4GAQ42mm1SIw5xgTkWc-~cw__&Key-Pair-Id=APKAI7S6A5RYOXBWRPDA"
-#tar -zxvf cellranger-3.1.0.tar.gz
-#wget https://cf.10xgenomics.com/supp/cell-exp/refdata-cellranger-GRCh38-3.0.0.tar.gz
-#tar -zxvf refdata-cellranger-GRCh38-3.0.0.tar.gz
-#Running Fastq file to matrix
-#/home/jimmy/Seurat/cellranger-3.0.2/cellranger(Path of cellranger) count --id=tiny_test #--fastqs=/home/jimmy/Seurat/Fastq(Path of Fastq(e.g.XXX_S1_L02_R2_001.fastq.gz )) #--transcriptome=/home/jimmy/Seurat/refdata-cellranger-GRCh38-3.0.0(GRch38 database path)
 options(repos=structure(c(CRAN="http://cran.mirrors.hoobly.com/")))
 install.packages("modeltools")
 install.packages("segmented")
