7d63f5d1e2136875d33228a9990a8e63e06566f3
ntukirafang <61813818+ntukirafang@users.noreply.github.com>
2024-04-18 15:50:21 +0800

    Update Seurat.R

--- Seurat.R
+++ Seurat.R
@@ -1,3 +1,4 @@
+#package install
 options(repos=structure(c(CRAN="http://cran.mirrors.hoobly.com/")))
 install.packages("modeltools")
 install.packages("segmented")
@@ -34,41 +35,61 @@ library(ggplot2)
 library(stringi)
 install_github("satijalab/seurat", force = TRUE)
 library(Seurat)
+# input and output pathway define
 setwd("C:/Users/jimmy.fang/Documents")
 devtools::install_github('satijalab/seurat-data')
 library(SeuratData)
+# Read Data from cell ranger analysis
 pbmc.data <- Read10X(data.dir = "C:/Users/jimmy.fang/Documents/test/")
+# Create object of single cell analysis
 pbmc <- CreateSeuratObject(counts = pbmc.data, project = "pbmc3k", min.cells = 3, min.features = 200)
+#Automatic estimate read count of mitochondria
 pbmc[["percent.mt"]] <- PercentageFeatureSet(pbmc, pattern = "^MT-")
+# Violine plot to present the read count 
 VlnPlot(pbmc, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3)
 plot1 <- FeatureScatter(pbmc, feature1 = "nCount_RNA", feature2 = "percent.mt")
 plot2 <- FeatureScatter(pbmc, feature1 = "nCount_RNA", feature2 = "nFeature_RNA")
 plot1 + plot2
+#Filter lower quality cell
 pbmc <- subset(pbmc, subset = nFeature_RNA > 200 & nFeature_RNA < 2500 & percent.mt < 5)
+# Normalize data
 pbmc <- NormalizeData(pbmc, normalization.method = "LogNormalize", scale.factor = 10000)
 pbmc <- NormalizeData(pbmc)
+# Find Variant feature and ploting
 pbmc <- FindVariableFeatures(pbmc, selection.method = "vst", nfeatures = 2000)
 top10 <- head(VariableFeatures(pbmc), 10)
 plot1 <- VariableFeaturePlot(pbmc)
 plot2 <- LabelPoints(plot = plot1, points = top10, repel = TRUE)
 plot1 + plot2
 all.genes <- rownames(pbmc)
+# Seurat integration
+features <- SelectIntegradtionFeatures(object.list = pbmc)
+pbmc.anchors <- FindiIntegrationAnchors(object.list = pbmc, dim = 1:20, anchor.features= features)
+pbmc <- IntegrateData(anchorset = pbmc.anchor, dim = 1:20)
+# Scale data
 pbmc <- ScaleData(pbmc, features = all.genes)
+# PCA analysis (Cell feature)
 pbmc <- RunPCA(pbmc, features = VariableFeatures(object = pbmc))
 print(pbmc[["pca"]], dims = 1:5, nfeatures = 5)
+# Three different plot shows the PCA result
 VizDimLoadings(pbmc, dims = 1:5, reduction = "pca")
 DimPlot(pbmc, reduction = "pca")
 DimHeatmap(pbmc, dims = 1, cells = 500, balanced = TRUE)
+# PCA score differentiation(Top 15)
 DimHeatmap(pbmc, dims = 1:15, cells = 500, balanced = TRUE)
 pbmc <- JackStraw(pbmc, num.replicate = 100)
 pbmc <- ScoreJackStraw(pbmc, dims = 1:20)
 JackStrawPlot(pbmc, dims = 1:15)
 ElbowPlot(pbmc)
+# Cell clustering (dims define the top 13th cluster to find)
 pbmc <- FindNeighbors(pbmc, dims = 1:13)
+# Cell cluster resolution(0.4-1.2 can much easier to different cell)
 pbmc <- FindClusters(pbmc, resolution = 0.5)
 head(Idents(pbmc), 5)
+# UMAP/tSNE find the cluster 
 pbmc <- RunUMAP(pbmc, dims = 1:13)
 DimPlot(pbmc, reduction = "umap")
+# Find marker gene of cell clustering
 cluster2.markers <- FindMarkers(pbmc, ident.1 = 2, min.pct = 0.25)
 head(cluster2.markers, n = 5)
 cluster5.markers <- FindMarkers(pbmc, ident.1 = 5, ident.2 = c(0, 3), min.pct = 0.25)
